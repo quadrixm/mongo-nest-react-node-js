64db33ca58fde590417ef6625fb5f09d1f69dce0
Mohammad Quadri <quadrixm@gmail.com>
2020-07-28 01:38:32 +0530

    Adding components and services classes

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RegisterComponent} from "./register/register.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RegisterComponent},
+  {path: 'dashboard', component: DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
