c160a467b6f55fb7abd61303cd54ad29e490b9f2
Mohammad Quadri <quadrixm@gmail.com>
2020-07-27 21:58:55 +0530

    Implementing services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+	async create(createUserDto: CreateUserDto): Promise<User> {
+		const user = new this.userModel(createUserDto);
+		return user.save();
+	}
+}
--- src/bookings/bookings.service.ts
+++ src/bookings/bookings.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Booking } from './schemas/booking.schema';
+import { CreateBookingDto } from './dto/create-booking.dto';
 
 @Injectable()
-export class BookingsService {}
+export class BookingsService {
+	constructor(@InjectModel(Booking.name) private bookingModel: Model<Booking>) {}
+
+	async create(createBookingDto: CreateBookingDto): Promise<Booking> {
+		const booking = new this.bookingModel(createBookingDto);
+		return booking.save();
+	}
+}
