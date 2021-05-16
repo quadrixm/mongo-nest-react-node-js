ed789b9297cb2b3194cecf65dce0d71c7a8a8f75
Mohammad Quadri <quadrixm@gmail.com>
2021-05-16 15:18:15 +0530

    few more changes

--- ui/src/app/auth.service.ts
+++ ui/src/app/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {CreateUserDto} from "./create-user.dto";
-import {API_BASE_URL} from "./constants";
-import {User} from "./user";
+import {HttpClient} from '@angular/common/http';
+import {CreateUserDto} from './create-user.dto';
+import {API_BASE_URL} from './constants';
+import {User} from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -18,14 +18,14 @@ export class AuthService {
   }
 
   public setUser(user: User) {
-    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem('user', JSON.stringify(user));
   }
 
   public getUser() {
-    return JSON.parse(localStorage.getItem("user"));
+    return JSON.parse(localStorage.getItem('user'));
   }
 
   public removeUser() {
-    return localStorage.removeItem("user");
+    return localStorage.removeItem('user');
   }
 }
